e4d64afa54d88d1ca3a17771e8b0b41d73519ac1
Herve Huneau <herve.huneau@prima.it>
2025-06-01 18:30:57 +0200

    add startWork as param for the command

--- plugin/torta-dolce.lua
+++ plugin/torta-dolce.lua
@@ -1,5 +1,9 @@
 vim.api.nvim_create_user_command("TortaDolce", function(opts)
 	package.loaded["torta-dolce"] = nil
 
-	require("torta-dolce").start_work()
-end, {})
+	if opts.args == "startWork" then
+		require("torta-dolce").start_work()
+	else
+		vim.notify("Only startWork implemented yet", vim.log.levels.WARN, {})
+	end
+end, { nargs = "+" })
