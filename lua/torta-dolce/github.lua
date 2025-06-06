0c4e5ca449204e9a5c553ea7126259f68a5ea637
Herve Huneau <herve.huneau@prima.it>
2025-06-06 14:39:14 +0200

    add get pull request WIP

--- lua/torta-dolce/github.lua
+++ lua/torta-dolce/github.lua
@@ -2,8 +2,10 @@ local M = {}
 local curl = require("plenary.curl")
 local config = require("torta-dolce.config")
 
+local GITHUB_API_URL = "https://api.github.com/repos/"
+
 function M.create_pull_request(repo, title, body, branch_name, base_branch_name, draft)
-	local result = curl.post("https://api.github.com/repos/" .. repo.owner .. "/" .. repo.repo .. "/pulls", {
+	local result = curl.post(GITHUB_API_URL .. repo.owner .. "/" .. repo.repo .. "/pulls", {
 		body = vim.json.encode({
 			title = title,
 			body = body,
@@ -28,6 +30,40 @@ function M.create_pull_request(repo, title, body, branch_name, base_branch_name,
 	return result
 end
 
-function M.get_pull_request(repo, branch_name) end
+-- TODO: present PR details that are intersting
+-- Like : state, is it draftable, any comments (filter only not resolved?), mergeable state
+function M.get_pull_request(repo, branch_name)
+	local result = curl.get(GITHUB_API_URL .. repo.owner .. "/" .. repo.repo .. "/pulls", {
+		query = {
+			head = repo.owner .. ":" .. branch_name,
+		},
+		headers = {
+
+			authorization = "Bearer " .. config.tokens.github,
+			["Accept"] = "application/vnd.github+json",
+			["X-GitHub-Api-Version"] = "2022-11-28",
+		},
+	})
+
+	-- This calls returns all matching PRs.
+	-- I just want the first most likely
+	local first_pr = result[1]
+	if not first_pr then
+		vim.notify("No PR found that matches current branch:" .. branch_name, vim.log.levels.INFO, {})
+		return
+	end
+
+	local pr_details_response =
+		curl.get(GITHUB_API_URL .. repo.owner .. "/" .. repo.repo .. "/pulls/" .. first_pr.pr_number, {
+			headers = {
+				authorization = "Bearer " .. config.tokens.github,
+				["Accept"] = "application/vnd.github.raw+json",
+				["X-GitHub-Api-Version"] = "2022-11-28",
+			},
+		})
+
+	pr_details_response = vim.fn.json_decode(pr_details_response.body)
+	print(vim.inspect(pr_details_response))
+end
 
 return M
